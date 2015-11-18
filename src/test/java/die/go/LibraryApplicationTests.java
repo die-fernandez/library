9111837c47e092ae77d1a26621d83b0ffa76bbbb
Diego Fernandez <diego.fernandez@synapse.com>
2015-11-18 17:52:49 -0300

    first commit

---
+++ src/test/java/die/go/LibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package die.go;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LibraryApplication.class)
+public class LibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
